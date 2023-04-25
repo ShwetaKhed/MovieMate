38663776abac6cf6d9eab675bcda5bd7705a5afd
nishaduhan <ndev0008@student.monash.edu>
2023-04-25 13:01:04 +1000

    WIP - Added Profile edit screen

--- app/src/main/java/com/example/moviemate/fragment/InfoFragment.java
+++ app/src/main/java/com/example/moviemate/fragment/InfoFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
@@ -14,12 +17,18 @@ import com.example.moviemate.viewmodel.SharedViewModel;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InfoFragment extends Fragment {
     private InfoFragmentBinding binding;
     public InfoFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
         // Inflate the View for this fragment using the binding
         binding = InfoFragmentBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
@@ -32,7 +41,57 @@ public class InfoFragment extends Fragment {
 
             }
         });
+
+        //Add genre spinner
+        createGenreSpinner();
+        createTheaterSpinner();
+
+        binding.buttonSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String newName=binding.editPersonName.getText().toString();
+                String newDob=binding.editDob.getText().toString();
+                String genrePreference = binding.spinnerGenre.getSelectedItem().toString();
+                String theaterPreference = binding.spinnerTheater.getSelectedItem().toString();
+
+            } });
         return view;
+    }
+
+    private void createGenreSpinner()
+    {
+        Spinner genreSpinner =binding.spinnerGenre;
+        List<String> list = new ArrayList<String>();
+        list.add("Comedy");
+        list.add("Horror");
+        list.add("Action");
+        list.add("Thriller");
+        list.add("Adventure");
+        list.add("Science fiction");
+        final ArrayAdapter<String> genreAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_item, list);
+        genreSpinner.setAdapter(genreAdapter);
+        genreSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+    private void createTheaterSpinner()
+    {
+        Spinner genreSpinner =binding.spinnerTheater;
+        List<String> list = new ArrayList<String>();
+        list.add("Henkel Street Cinema");
+        list.add("Mystery Radio Theatre");
+        list.add("HOYTS Melbourne Central");
+        list.add("Palace Cinema Como");
+
+        final ArrayAdapter<String> theaterAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_item, list);
+        genreSpinner.setAdapter(theaterAdapter);
     }
     @Override
     public void onDestroyView() {
