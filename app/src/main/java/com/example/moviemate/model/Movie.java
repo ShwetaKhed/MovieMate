d9e3b6e0b9f07e81eb359122ec8e4172c04a8f0c
ShwetaK <skhe008@student.monash.edu>
2023-04-28 01:08:33 +1000

    Added datasource

--- app/src/main/java/com/example/moviemate/model/Movie.java
+++ app/src/main/java/com/example/moviemate/model/Movie.java
@@ -1,17 +1,52 @@
 package com.example.moviemate.model;
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Movie {
     @SerializedName("original_title")
     private String originalTitle;
 
+    @SerializedName("poster_path")
+    private String posterPath;
 
-    public Movie(String originalTitle) {
+    @SerializedName("overview")
+    private String overview;
+
+    @SerializedName("release_date")
+    private String releaseDate;
+
+
+    public void setOriginalTitle(String originalTitle) {
         this.originalTitle = originalTitle;
     }
 
     public String getOriginalTitle() {
         return originalTitle;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
     }
 }
