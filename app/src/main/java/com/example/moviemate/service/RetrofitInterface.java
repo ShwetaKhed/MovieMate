5c6710282870dc31cda238de7dbb1fef3494409a
ShwetaK <skhe008@student.monash.edu>
2023-05-05 13:29:21 +1000

    Errors for datepicker

--- app/src/main/java/com/example/moviemate/service/RetrofitInterface.java
+++ app/src/main/java/com/example/moviemate/service/RetrofitInterface.java
@@ -5,6 +5,7 @@ import com.example.moviemate.model.MovieResult;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -18,5 +19,8 @@ public interface RetrofitInterface {
 
     @GET("movie/top_rated?api_key=6f6d1b438fddb937dd48a7f88b87eae7&page=2")
     Call<MovieResult> getPopularMovies();
+
+    @GET("movie/top_rated")
+    Call<MovieResult> getPopularMovies1(@Query("api_key") String api_key, @Query("page") String page);
 
 }
